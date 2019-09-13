fe5c354ca62065c64e6b31857f2e4ea7c1a5bea3
Vince Brown <vince@vincebrown.me>
2019-09-12 21:48:55 -0700

    Renamed page to /testimonials

--- pages/testimonials.js
+++ pages/testimonials.js
@@ -1,11 +1,11 @@
 // Components
 import PageIntro from '../components/PageIntro'
 
-const WhyOurPartnersLoveUs = () => (
+const Testimonials = () => (
   <PageIntro
     heading="Why our partners love us"
     blurb="Collaboration, commitment, and design that delivers results keeps our partners raving about us."
   />
 )
 
-export default WhyOurPartnersLoveUs
+export default Testimonials
