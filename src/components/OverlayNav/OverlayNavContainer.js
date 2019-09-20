37377e8588e5c9ec80be440bcf000ec1a5175ddc
Vince Brown <vince@vincebrown.me>
2019-09-20 02:15:50 -0700

    Added basic animation, to overlay navigation

--- src/components/OverlayNav/OverlayNavContainer.js
+++ src/components/OverlayNav/OverlayNavContainer.js
@@ -1,18 +1,17 @@
 // Packages
 import styled from 'styled-components'
+import { animated } from 'react-spring'
 
-const OverlayNavContainer = styled.div`
+const OverlayNavContainer = styled(animated.div)`
   position: fixed;
   top: 0;
-  left: 0;
   width: 100%;
   height: 100%;
   color: ${props => props.theme.white};
   background-color: ${props => props.theme.darkBlue};
-  font-size: ${props => props.theme.f2};
+  font-size: 32px;
   font-weight: 800;
   letter-spacing: 0.2px;
-  display: ${props => (props.isOpen ? 'block' : 'none')};
   padding: 2.1875rem 2.5rem;
 
   .CloseIcon {
@@ -21,11 +20,12 @@ const OverlayNavContainer = styled.div`
   }
 
   .OverlayNavList {
-    padding-top: 7.5rem;
+    padding-top: 11rem;
   }
 
   .OverlayNavLink {
     margin-bottom: 3rem;
+    position: relative;
   }
 `
 
--- src/components/OverlayNav/index.js
+++ src/components/OverlayNav/index.js
@@ -2,28 +2,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
+import { animated, useSpring, config } from 'react-spring'
 
 // Styled Components
 import OverlayNavContainer from './OverlayNavContainer'
 
-const OverlayNav = ({ isOpen }) => (
-  <OverlayNavContainer isOpen={isOpen}>
-    <ul className="OverlayNavList">
-      <li className="OverlayNavLink">
-        <Link to="/who-we-are">Who We Are</Link>
-      </li>
-      <li className="OverlayNavLink">
-        <Link to="/what-we-do">What We Do</Link>
-      </li>
-      <li className="OverlayNavLink">
-        <Link to="/case-studies">Case Studies</Link>
-      </li>
-      <li>
-        <Link to="/case-studies">Get in touch</Link>
-      </li>
-    </ul>
-  </OverlayNavContainer>
-)
+const OverlayNav = ({ isOpen }) => {
+  const animationStyles = useSpring({
+    opacity: isOpen ? 1 : 0,
+    bottom: isOpen ? '0px' : '-10px',
+    delay: 200,
+    config: config.slow,
+  })
+
+  const overlayNavContainerAnimation = useSpring({
+    left: isOpen ? '0' : '-100%',
+  })
+
+  return (
+    <OverlayNavContainer isOpen={isOpen} style={overlayNavContainerAnimation}>
+      <ul className="OverlayNavList">
+        <animated.li className="OverlayNavLink" style={animationStyles}>
+          <Link to="/who-we-are">Who we are</Link>
+        </animated.li>
+        <animated.li className="OverlayNavLink" style={animationStyles}>
+          <Link to="/what-we-do">What we do</Link>
+        </animated.li>
+        <animated.li className="OverlayNavLink" style={animationStyles}>
+          <Link to="/case-studies">Case Studies</Link>
+        </animated.li>
+        <animated.li className="OverlayNavLink" style={animationStyles}>
+          <Link to="/case-studies">Get in touch</Link>
+        </animated.li>
+      </ul>
+    </OverlayNavContainer>
+  )
+}
 
 OverlayNav.propTypes = {
   isOpen: PropTypes.bool.isRequired,
