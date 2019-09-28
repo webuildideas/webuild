dbdfbb78477583b9d7ee30bef16ae471c276969b
Vince Brown <vince@vincebrown.me>
2019-09-27 19:19:58 -0700

    finished writing the Query for the case study page

--- src/pages/case-studies.js
+++ src/pages/case-studies.js
@@ -1,5 +1,7 @@
 // Packages
 import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
 
 // Styled Components
 import SiteMaxWidthContainer from '../components/Shared/SiteMaxWidthContainer'
@@ -10,7 +12,7 @@ import Contact from '../components/Contact'
 import Meta from '../components/Meta'
 import Footer from '../components/Footer'
 
-const CaseStudies = () => (
+const CaseStudies = ({ data }) => (
   <>
     <Meta title="Case Studies" />
     <SiteMaxWidthContainer>
@@ -18,10 +20,37 @@ const CaseStudies = () => (
         heading="We help our partners solve <br> ambitious design challenges"
         blurb="We partner with inspiring entrepreneurs and values-driven companies to design and create world-class digital products, tools and experiences."
       />
+      {data.allContentfulCaseStudy.edges.map(edge => (
+        <p>{edge.node.title}</p>
+      ))}
       <Contact />
     </SiteMaxWidthContainer>
     <Footer />
   </>
 )
+
+CaseStudies.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export const CASE_STUDIES_LISTING_QUERY = graphql`
+  query caseStudiesListingQuery {
+    allContentfulCaseStudy(sort: { order: ASC, fields: createdAt }) {
+      edges {
+        node {
+          tagline
+          title
+          listingImage {
+            fluid {
+              srcSet
+              src
+              sizes
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default CaseStudies
