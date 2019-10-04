f53400d6f81b94b45d16586f08677576489135c8
Vince Brown <vince@vincebrown.me>
2019-10-03 21:29:47 -0700

    finished footer styles v1

--- src/components/Footer/FooterContainer.js
+++ src/components/Footer/FooterContainer.js
@@ -6,15 +6,18 @@ import { rhythmUnit } from '../../utils/typography'
 
 const FooterContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   flex-wrap: wrap;
   max-width: 1100px;
   margin: ${() => `0 auto ${rhythmUnit(1.75)}`};
   padding: ${() => `0 ${rhythmUnit(1)}`};
   font-size: ${props => props.theme.f7};
   text-transform: uppercase;
+  font-weight: 800;
   color: ${props => props.theme.grey};
-
+  @media (min-width: 768px) {
+    justify-content: space-between;
+  }
   @media (min-width: 1164px) {
     padding: 0;
   }
@@ -35,6 +38,10 @@ const FooterContainer = styled.div`
     width: 100%;
     text-align: center;
     margin-bottom: ${() => rhythmUnit(1)};
+    @media (min-width: 1100px) {
+      position: relative;
+      left: 80px;
+    }
   }
 
   .follow-social {
