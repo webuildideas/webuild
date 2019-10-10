cd3f5a1b8840784faaea7cf6b691e6505a794c57
Vince Brown <vince@vincebrown.me>
2019-10-09 19:26:40 -0700

    Dynamically Generating Case Studies
    Updated gatsby-node to dynamically create all our case study detail pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,30 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  const CaseStudyDetail = path.resolve('./src/components/CaseStudyDetail')
+  return new Promise(resolve => {
+    graphql(`
+      {
+        allContentfulCaseStudy {
+          edges {
+            node {
+              slug
+            }
+          }
+        }
+      }
+    `).then(results => {
+      results.data.allContentfulCaseStudy.edges.forEach(({ node }) => {
+        createPage({
+          path: `/case-studies/${node.slug}`,
+          component: CaseStudyDetail,
+          context: {
+            slug: node.slug,
+          },
+        })
+      })
+      resolve()
+    })
+  })
+}
---
+++ src/components/CaseStudyDetail/index.js
@@ -0,0 +1,7 @@
+// Packages
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+const CaseStudyDetail = () => <h1>Case Study Detail Page</h1>
+
+export default CaseStudyDetail
