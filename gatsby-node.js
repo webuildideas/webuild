9c9e62941f51c6a81e4f0cc6c8af1fd2549aec85
Vince Brown <vince@vincebrown.me>
2019-10-09 19:42:17 -0700

    Update gatsby-node.js
    Updated gatsby node to use async/await instead of returning a promise

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,29 +1,27 @@
 const path = require('path')
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  return new Promise((resolve, reject) => {
-    graphql(`
-      {
-        allContentfulCaseStudy {
-          edges {
-            node {
-              slug
-            }
+
+  const result = await graphql(`
+    {
+      allContentfulCaseStudy {
+        edges {
+          node {
+            slug
           }
         }
       }
-    `).then(results => {
-      results.data.allContentfulCaseStudy.edges.forEach(({ node }) => {
-        createPage({
-          path: `/case-studies/${node.slug}`,
-          component: path.resolve('./src/components/CaseStudyDetail/index.js'),
-          context: {
-            slug: node.slug,
-          },
-        })
-      })
-      resolve()
+    }
+  `)
+
+  result.data.allContentfulCaseStudy.edges.forEach(({ node }) => {
+    createPage({
+      path: `/case-studies/${node.slug}`,
+      component: path.resolve('./src/components/CaseStudyDetail/index.js'),
+      context: {
+        slug: node.slug,
+      },
     })
   })
 }
