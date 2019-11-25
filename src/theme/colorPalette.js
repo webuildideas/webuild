f96623f3d9d83e033501c50a4851fe249d478b43
Vince Brown <vince@vincebrown.me>
2019-11-24 22:36:12 -0800

    Updated all packages, and added new color palette

--- src/theme/colorPalette.js
+++ src/theme/colorPalette.js
@@ -1,6 +1,18 @@
 export const colorPalette = {
   black: '#0E0E1B',
   blackHover: '#3E3E49',
+
+  // New Color Palette
+  vulcan: '#0E0E1B',
+  tuna: '#3E3E49',
+  comet: '#53536A',
+  stormGrey: '#757588',
+  bisonHide: '#C7B2AB',
+  coldTurkey: '#D2C1BC',
+  concrete: '#F3F3F3',
+  snow: '#F9F9F9',
+
+  // Old Color Palette
   grey: '#7B8D9D',
   greyHover: '#95A4B1',
   lightGrey: '#E4E4F3',
