e00084385324197f6d1d4354c22c74890fe23230
Vince Brown <vince@vincebrown.me>
2019-12-21 00:00:35 -0800

    Fixed styling of DesignPartner
    Adjusted the grid layout of DesignPartner and finalized mobile - desktop styling.

--- src/components/DesignPartner/style.js
+++ src/components/DesignPartner/style.js
@@ -28,17 +28,37 @@ export const DesignPartner = styled.section`
 export const DesignPartnerGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr;
-  grid-gap: ${() => rhythmUnit(2.75)};
-  @media (min-width: 768px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
+  grid-gap: ${() => rhythmUnit(1)};
   svg {
     margin-bottom: ${() => rhythmUnit(1)};
+    width: 60px;
   }
 
   h3 {
     text-transform: uppercase;
     font-weight: 800;
     margin-bottom: ${() => rhythmUnit(0.25)};
+  }
+
+  @media (min-width: 850px) {
+    grid-template-columns: repeat(3, 1fr);
+    svg {
+      width: 50px;
+    }
+    p {
+      font-size: ${props => props.theme.f4};
+    }
+  }
+  @media (min-width: 1024px) {
+    grid-gap: ${() => rhythmUnit(2)};
+    svg {
+      width: 60px;
+    }
+    p {
+      font-size: ${props => props.theme.f3};
+    }
+  }
+  @media (min-width: 1200px) {
+    grid-gap: ${() => rhythmUnit(2.75)};
   }
 `
