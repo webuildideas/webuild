33f0f6345723efb4c67d64e2f7235ab6318c9ce8
Vince Brown <vince@vincebrown.me>
2019-12-29 00:03:02 -0800

    Removed Max Width on Button
    Removed the max width style on Button component that was causing wrapping on smaller screen sizes.

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -41,7 +41,6 @@ export const PrimaryButton = styled.a`
   text-transform: uppercase;
   line-height: 1;
   padding: ${() => ` ${rhythmUnit(0.5)} ${rhythmUnit(1)}`};
-  max-width: 14rem;
   letter-spacing: 0.05em;
   border: 1px solid ${props => props.theme.vulcan};
   color: ${props => props.theme.vulcan};
