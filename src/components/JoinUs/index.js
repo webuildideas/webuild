9f3b677f60fb042a0c775c87eb9a94d67f23877e
Vince Brown <vince@vincebrown.me>
2020-01-12 20:37:44 -0800

    Animated JoinUs

--- src/components/JoinUs/index.js
+++ src/components/JoinUs/index.js
@@ -1,5 +1,7 @@
 // Packages
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
+import { motion, useAnimation } from 'framer-motion'
 
 // Components
 import Button from '../Button'
@@ -9,41 +11,125 @@ import * as S from './style'
 import SiteMaxWidthContainer from '../Shared/SiteMaxWidthContainer'
 import SectionHeading from '../Shared/SectionHeading'
 
-const JoinUs = () => (
-  <S.JoinUs>
-    <SiteMaxWidthContainer>
-      <SectionHeading>
-        <h1 className="SectionHeading__title">
-          Join us and work from anywhere
-        </h1>
-        <h2 className="SectionHeading__subtitle">
-          We’re always looking for inspiring, down to earth, and talented people
-          to join our amazing remote team.
-        </h2>
-      </SectionHeading>
-      <S.JoinUsJobs>
-        <S.JoinUsJob>
-          <h3>Product Designer</h3>
-          <p>Remote (US Preferred)</p>
-        </S.JoinUsJob>
-        <S.JoinUsJob>
-          <h3>Frontend Developer</h3>
-          <p>Remote (US Preferred)</p>
-        </S.JoinUsJob>
-        <S.JoinUsJob>
-          <h3>Product Manager</h3>
-          <p>Remote (US Preferred)</p>
-        </S.JoinUsJob>
-      </S.JoinUsJobs>
-      <Button
-        href="https://forms.gle/FvoC68UKVzXMKdjy9"
-        target="_blank"
-        type="secondaryButton"
-      >
-        ✌️Introduce Yourself
-      </Button>
-    </SiteMaxWidthContainer>
-  </S.JoinUs>
-)
+const JoinUs = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.9,
+    triggerOnce: true,
+  })
+
+  const controls = useAnimation()
+
+  const variants = {
+    visible: i => ({
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.55,
+        delay: i * 0.152,
+        type: 'spring',
+      },
+    }),
+    hidden: {
+      y: 15,
+      opacity: 0,
+    },
+  }
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible')
+    }
+  }, [controls, inView])
+  return (
+    <S.JoinUs ref={ref}>
+      <SiteMaxWidthContainer>
+        <SectionHeading>
+          <motion.h1
+            animate={controls}
+            className="SectionHeading__title"
+            custom={0}
+            initial="hidden"
+            variants={variants}
+          >
+            Join us and work from anywhere
+          </motion.h1>
+          <motion.h2
+            animate={controls}
+            className="SectionHeading__subtitle"
+            custom={1}
+            initial="hidden"
+            variants={variants}
+          >
+            We’re always looking for inspiring, down to earth, and talented
+            people to join our amazing remote team.
+          </motion.h2>
+        </SectionHeading>
+        <S.JoinUsJobs>
+          <S.JoinUsJob>
+            <motion.h3
+              animate={controls}
+              custom={2}
+              initial="hidden"
+              variants={variants}
+            >
+              Product Designer
+            </motion.h3>
+            <motion.p
+              animate={controls}
+              custom={3}
+              initial="hidden"
+              variants={variants}
+            >
+              Remote (US Preferred)
+            </motion.p>
+          </S.JoinUsJob>
+          <S.JoinUsJob>
+            <motion.h3
+              animate={controls}
+              custom={4}
+              initial="hidden"
+              variants={variants}
+            >
+              Frontend Developer
+            </motion.h3>
+            <motion.p
+              animate={controls}
+              custom={5}
+              initial="hidden"
+              variants={variants}
+            >
+              Remote (US Preferred)
+            </motion.p>
+          </S.JoinUsJob>
+          <S.JoinUsJob>
+            <motion.h3
+              animate={controls}
+              custom={6}
+              initial="hidden"
+              variants={variants}
+            >
+              Product Manager
+            </motion.h3>
+            <motion.p
+              animate={controls}
+              custom={7}
+              initial="hidden"
+              variants={variants}
+            >
+              Remote (US Preferred)
+            </motion.p>
+          </S.JoinUsJob>
+        </S.JoinUsJobs>
+        <Button
+          href="https://forms.gle/FvoC68UKVzXMKdjy9"
+          target="_blank"
+          type="secondaryButton"
+        >
+          ✌️Introduce Yourself
+        </Button>
+      </SiteMaxWidthContainer>
+    </S.JoinUs>
+  )
+}
 
 export default JoinUs
