9909f6fe0c0b4d64474d16fcb2608030d0c1a7f4
Vince Brown <vince@vincebrown.me>
2020-10-14 06:19:22 -0700

    Updated BlogPost query

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -21,6 +21,10 @@ export const query = graphql`
   query blogPostQuery($slug: String!) {
     contentfulBlogPost(slug: { eq: $slug }) {
       title
+      content
+      publishDate
+      readNext
+      topic
     }
   }
 `
