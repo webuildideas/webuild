c7c5cb7dc276ca2ed82b820bb6e82a3ff58eb52d
Vince Brown <vince@vincebrown.me>
2020-10-16 07:05:19 -0700

    Added styling and basic rendering for blog post

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,10 +1,14 @@
 // Packages
 import React from 'react'
+import dayjs from 'dayjs'
+import { renderRichText } from 'gatsby-source-contentful/rich-text'
+import { BLOCKS } from '@contentful/rich-text-types'
+import { Options } from '@contentful/rich-text-react-renderer'
 import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 // Commons
 import { TypeBlogPost } from '../common/types/BlogPost'
-import SiteMaxWidthContainer from '../common/styledComponents/SiteMaxWidthContainer'
 
 interface Props {
   data: {
@@ -12,11 +16,42 @@ interface Props {
   }
 }
 
+const options: Options = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: (node) => {
+      return <Img durationFadeIn={125} fadeIn fluid={node.data.target.fluid} />
+    }
+  }
+}
+
 const BlogPost = ({ data: { contentfulBlogPost: blogPost } }: Props) => {
   return (
-    <SiteMaxWidthContainer>
-      <h1 className="text-xl">{blogPost.title}</h1>
-    </SiteMaxWidthContainer>
+    <article className="prose mx-auto pt-16 pb-12 px-6 md:px-0">
+      <h1 className="text-xl mb-12">{blogPost.title}</h1>
+      <div className="flex items-center">
+        {blogPost.author
+          ? blogPost.author.map((author, idx) => {
+              return (
+                <>
+                  <Img
+                    alt={`${author.name} Headshot`}
+                    className="rounded-full mr-4 inline-block"
+                    durationFadeIn={150}
+                    fadeIn
+                    fixed={author.headshot.fixed}
+                    imgStyle={{ marginTop: 0, marginBottom: 0 }}
+                  />
+                  <p key={`${author.name}-${idx}`} className="inline">
+                    by {author.name} on{' '}
+                    {dayjs(blogPost.publishDate).format('MMMM DD, YYYY')}
+                  </p>
+                </>
+              )
+            })
+          : null}
+      </div>
+      {blogPost.content ? renderRichText(blogPost.content, options) : null}
+    </article>
   )
 }
 
@@ -24,13 +59,21 @@ export const query = graphql`
   query blogPostQuery($slug: String!) {
     contentfulBlogPost(slug: { eq: $slug }) {
       title
+      author {
+        name
+        headshot {
+          fixed(cropFocus: FACE, height: 65, width: 65) {
+            ...GatsbyContentfulFixed_withWebp_noBase64
+          }
+        }
+      }
       content {
         raw
         references {
           contentful_id
           ... on ContentfulAsset {
             id
-            fluid(maxWidth: 1100) {
+            fluid(maxWidth: 800) {
               ...GatsbyContentfulFluid_withWebp_noBase64
             }
           }
