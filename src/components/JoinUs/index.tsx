77791285ff3829ab3d98cbe3eaa70cb2fe2495ac
Vince Brown <vince@vincebrown.me>
2020-10-19 07:29:28 -0700

    Added new Content type Job and update Join Us  section

--- src/components/JoinUs/index.tsx
+++ src/components/JoinUs/index.tsx
@@ -1,7 +1,8 @@
 // Packages
 import React, { useEffect } from 'react'
+import { OutboundLink } from 'gatsby-plugin-gtag'
 import { useInView } from 'react-intersection-observer'
-import { motion, useAnimation } from 'framer-motion'
+import { motion, useAnimation, Variants } from 'framer-motion'
 
 // Commons
 import * as S from './style'
@@ -10,43 +11,47 @@ import '../../common/styles/SectionHeading.css'
 
 // Components
 import Button from '../Button'
+import { TypeJob } from '../../common/types/Job'
 
-const JoinUs = () => {
+interface Props {
+  jobs: TypeJob[]
+}
+
+const variants: Variants = {
+  visible: (i: number) => ({
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.55,
+      delay: i * 0.175,
+      type: 'spring'
+    }
+  }),
+  hidden: {
+    y: 15,
+    opacity: 0
+  }
+}
+
+const JoinUs = ({ jobs }: Props) => {
+  const animationControls = useAnimation()
   const [ref, inView] = useInView({
     triggerOnce: true,
     threshold: 0.75
   })
 
-  const controls = useAnimation()
-
-  const variants = {
-    visible: (i: number) => ({
-      y: 0,
-      opacity: 1,
-      transition: {
-        duration: 0.55,
-        delay: i * 0.175,
-        type: 'spring'
-      }
-    }),
-    hidden: {
-      y: 15,
-      opacity: 0
-    }
-  }
-
   useEffect(() => {
     if (inView) {
-      controls.start('visible')
+      animationControls.start('visible')
     }
-  }, [controls, inView])
+  }, [animationControls, inView])
 
   return (
-    <S.JoinUs ref={ref} data-testid="joinUs">
+    <section ref={ref} data-testid="joinUs">
       <SiteMaxWidthContainer>
         <div>
           <motion.h1
-            animate={controls}
+            animate={animationControls}
             className="SectionHeading__title"
             custom={1}
             data-testid="joinUs-title"
@@ -56,7 +61,7 @@ const JoinUs = () => {
             Join us and work from anywhere
           </motion.h1>
           <motion.h2
-            animate={controls}
+            animate={animationControls}
             className="SectionHeading__subtitle"
             custom={2}
             data-testid="joinUs-subtitle"
@@ -67,52 +72,41 @@ const JoinUs = () => {
             people to join our amazing remote team.
           </motion.h2>
         </div>
-        <S.JoinUsJobs data-testid="joinUs-jobs">
-          <S.JoinUsJob
-            href="https://webuildideas.bamboohr.com/jobs/view.php?id=22"
-            rel="noopener noreferrer"
-            target="_blank"
+        {jobs.length > 0 ? (
+          <div
+            className="grid grid-cols-1 md:grid-cols-3 justify-between gap-3 my-12"
+            data-testid="joinUs-jobs"
           >
-            <motion.h3
-              animate={controls}
-              custom={2}
-              initial="hidden"
-              variants={variants}
-            >
-              Product Designer
-            </motion.h3>
-            <motion.p
-              animate={controls}
-              custom={3}
-              initial="hidden"
-              variants={variants}
-            >
-              Remote (US Preferred)
-            </motion.p>
-          </S.JoinUsJob>
-          <S.JoinUsJob
-            href="https://webuildideas.bamboohr.com/jobs/view.php?id=21"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <motion.h3
-              animate={controls}
-              custom={4}
-              initial="hidden"
-              variants={variants}
-            >
-              Product Manager
-            </motion.h3>
-            <motion.p
-              animate={controls}
-              custom={5}
-              initial="hidden"
-              variants={variants}
-            >
-              Remote (US Preferred)
-            </motion.p>
-          </S.JoinUsJob>
-        </S.JoinUsJobs>
+            {jobs.map((job: TypeJob, idx: number) => {
+              return (
+                <OutboundLink
+                  key={`job-${idx}`}
+                  href={job.applicationLink}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <motion.h3
+                    animate={animationControls}
+                    className="uppercase mb-3"
+                    custom={idx + 2}
+                    initial="hidden"
+                    variants={variants}
+                  >
+                    {job.title}
+                  </motion.h3>
+                  <motion.p
+                    animate={animationControls}
+                    custom={idx + 3}
+                    initial="hidden"
+                    variants={variants}
+                  >
+                    {job.location}
+                  </motion.p>
+                </OutboundLink>
+              )
+            })}
+          </div>
+        ) : null}
         <Button
           href="https://webuildideas.bamboohr.com/jobs/"
           rel="noopener noreferrer"
@@ -122,7 +116,7 @@ const JoinUs = () => {
           View Open Positions
         </Button>
       </SiteMaxWidthContainer>
-    </S.JoinUs>
+    </section>
   )
 }
 
