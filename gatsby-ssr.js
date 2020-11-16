694132a37419c9aa8764f7baee42feaf58074121
Vince Brown <vince@vincebrown.me>
2020-11-16 07:50:44 -0800

    wrapped ssr in recoil root

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,15 @@
 // Packages
 import React from 'react'
+import { RecoilRoot } from 'recoil'
 
 // Components
 import AppProvider from './src/components/AppProvider'
 
 export const wrapPageElement = ({ element, props }) => {
-  return <AppProvider {...props}>{element}</AppProvider>
+  return (
+    <RecoilRoot>
+      <AppProvider {...props}>{element}</AppProvider>
+    </RecoilRoot>
+  )
 }
-
 // export { wrapRootElement } from './src/common/apollo/wrap-root-element'
