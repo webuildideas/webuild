e93ba13cf11519448d6bd6285ab10cc157d3fc68
Vince Brown <vince@vincebrown.me>
2020-11-16 07:30:30 -0800

    Updated client endpoint

--- src/common/apollo/client.ts
+++ src/common/apollo/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:8000/___graphql?',
+  uri: `${window.location.origin}/___graphql?`,
   cache: new InMemoryCache()
 })
