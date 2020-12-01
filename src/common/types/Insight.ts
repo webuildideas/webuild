685ff6f9714b8ef89bdb5055b203d8acec590857
Vince Brown <vince@vincebrown.me>
2020-12-01 07:16:05 -0800

    Removed unused types and old Blog Post template

--- src/common/types/Insight.ts
+++ src/common/types/Insight.ts
@@ -1,6 +1,5 @@
 import { Document } from '@contentful/rich-text-types'
 import { TypeAuthor } from './Author'
-// import { TypeTopic } from './Topic'
 
 export type InsightType = 'Article' | 'White Paper'
 export interface TypeInsight {
