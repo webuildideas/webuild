f8ad55dcb684e8d4586f8f4e082e235eb3f04126
Vince Brown <vince@vincebrown.me>
2021-01-24 08:20:34 -0800

    Finished config for insightTopicIconConfig

--- src/common/types/Insight.ts
+++ src/common/types/Insight.ts
@@ -1,11 +1,33 @@
 // Packages
+import { RenderRichTextData } from 'gatsby-source-contentful/rich-text'
+
 // Common
 import { TypeAuthor } from '@common/types/Author'
-import { RenderRichTextData } from 'gatsby-source-contentful/rich-text'
-import { TypeCarousel } from './Carousel'
-import { TypeContentfulAsset } from './Contentful'
+import { TypeCarousel } from '@common/types/Carousel'
+import { TypeContentfulAsset } from '@common/types/Contentful'
 
-export type InsightType = 'Article' | 'White Paper'
+export type InsightType =
+  | 'Article'
+  | 'White Paper'
+  | 'Email Course'
+  | 'Event'
+  | 'Podcast'
+  | 'Publication'
+  | 'Video'
+  | 'Webinar'
+
+export type InsightTopic =
+  | 'clients'
+  | 'design'
+  | 'digital'
+  | 'expertise'
+  | 'growth'
+  | 'management'
+  | 'marketing'
+  | 'processes'
+  | 'roles'
+  | 'team'
+
 export interface TypeInsight {
   type: InsightType
   title: string
@@ -16,7 +38,7 @@ export interface TypeInsight {
   readNext?: TypeInsight[]
   author: TypeAuthor
   hashtags?: string[]
-  topics?: string[]
+  topics?: InsightTopic[]
   shareQuote?: {
     shareQuote: string
   }
--- src/components/Insights/InsightListing/config.ts
+++ src/components/Insights/InsightListing/config.ts
@@ -1,17 +1,34 @@
 // Common
-import { InsightType } from '@common/types/Insight'
+import { InsightTopic, InsightType } from '@common/types/Insight'
 import { WithClassName } from '@common/types/Utilities'
 
 // Assets
 import Article from '@static/svgs/type/article.inline.svg'
 import WhitePaper from '@static/svgs/type/white-paper.inline.svg'
+import EmailCourse from '@static/svgs/type/email-course.inline.svg'
+import Event from '@static/svgs/type/event.inline.svg'
+import Podcast from '@static/svgs/type/podcast.inline.svg'
+import Publication from '@static/svgs/type/public.inline.svg'
+import Video from '@static/svgs/type/video.inline.svg'
+import Webinar from '@static/svgs/type/webinar.inline.svg'
 
-type InsightTypeIconPayload = {
+import Clients from '@static/svgs/type/clients.inline.svg'
+import Design from '@static/svgs/type/design.inline.svg'
+import Digital from '@static/svgs/type/digital.inline.svg'
+import Expertise from '@static/svgs/type/expertise.inline.svg'
+import Growth from '@static/svgs/type/growth.inline.svg'
+import Management from '@static/svgs/type/management.inline.svg'
+import Marketing from '@static/svgs/type/marketing.inline.svg'
+import Processes from '@static/svgs/type/processes.inline.svg'
+import Roles from '@static/svgs/type/roles.inline.svg'
+import Team from '@static/svgs/type/team.inline.svg'
+
+type IconPayload = {
   icon: React.FC<WithClassName>
   name: string
 }
 
-type InsightTypeIconConfig = Record<InsightType, InsightTypeIconPayload>
+type InsightTypeIconConfig = Record<InsightType, IconPayload>
 
 export const insightTypeIconConfig: InsightTypeIconConfig = {
   Article: {
@@ -21,5 +38,74 @@ export const insightTypeIconConfig: InsightTypeIconConfig = {
   'White Paper': {
     icon: WhitePaper,
     name: 'White Paper'
+  },
+  'Email Course': {
+    icon: EmailCourse,
+    name: 'Email Course'
+  },
+  Event: {
+    icon: Event,
+    name: 'Event'
+  },
+  Podcast: {
+    icon: Podcast,
+    name: 'Podcast'
+  },
+  Publication: {
+    icon: Publication,
+    name: 'Publication'
+  },
+  Video: {
+    icon: Video,
+    name: 'Video'
+  },
+  Webinar: {
+    icon: Webinar,
+    name: 'Webinar'
+  }
+}
+
+type InsightTopicIconConfig = Record<InsightTopic, IconPayload>
+
+export const insightTopicIconConfig: InsightTopicIconConfig = {
+  clients: {
+    icon: Clients,
+    name: 'Client'
+  },
+  design: {
+    icon: Design,
+    name: 'Design'
+  },
+  digital: {
+    icon: Digital,
+    name: 'Digital'
+  },
+  expertise: {
+    icon: Expertise,
+    name: 'Expertise'
+  },
+  growth: {
+    icon: Growth,
+    name: 'Growth'
+  },
+  management: {
+    icon: Management,
+    name: 'Management'
+  },
+  marketing: {
+    icon: Marketing,
+    name: 'Marketing'
+  },
+  processes: {
+    icon: Processes,
+    name: 'Processes'
+  },
+  roles: {
+    icon: Roles,
+    name: 'Roles'
+  },
+  team: {
+    icon: Team,
+    name: 'Team'
   }
 }
