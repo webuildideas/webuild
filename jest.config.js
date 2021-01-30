3d1386d454a1fcdf0d17d1f3008284daa3f10ecd
Vince Brown <vince@vincebrown.me>
2021-01-30 07:02:17 -0800

    Added new example data

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,8 @@ module.exports = {
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
     '^@common/(.*)$': '<rootDir>/src/common/$1',
     '^@components/(.*)$': '<rootDir>/src/components/$1',
-    '^@static/(.*)$': '<rootDir>/src/static/$1'
+    '^@static/(.*)$': '<rootDir>/src/static/$1',
+    '^@exampleData/(.*)$': '<rootDir>/src/exampleData/$1'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
--- src/components/Insights/InsightListing/index.spec.tsx
+++ src/components/Insights/InsightListing/index.spec.tsx
@@ -1,35 +1,12 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { TypeInsight } from '@common/types/Insight'
+import { INSIGHT_ARTICLE } from '@exampleData/Insight'
 import InsightListing from '.'
-
-const INSIGHT: TypeInsight = {
-  type: 'Article',
-  topics: ['clients'],
-  title: 'Amazing article about design',
-  slug: 'amazing-article',
-  publishDate: '02-22-2021',
-  content: {
-    raw: 'Content of this article!',
-    references: []
-  },
-  author: {
-    name: 'Joe Rogan',
-    headshot: {
-      fixed: {
-        width: 50,
-        height: 50,
-        src: 'headshot-src.jpg',
-        srcSet: 'headshot-srcset.jpg'
-      }
-    }
-  }
-}
 
 describe('<InsightListing />', () => {
   it('renders correctly', () => {
     const { getByTestId, getByText } = render(
-      <InsightListing insight={INSIGHT} />
+      <InsightListing insight={INSIGHT_ARTICLE} />
     )
 
     const title = getByText('Amazing article about design')
