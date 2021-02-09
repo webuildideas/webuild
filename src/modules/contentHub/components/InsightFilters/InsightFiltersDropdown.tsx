f7a84a1998707cc19dde38051af19c748e636e1e
Vince Brown <vince@vincebrown.me>
2021-02-09 07:32:42 -0800

    finished styling

--- src/modules/contentHub/components/InsightFilters/InsightFiltersDropdown.tsx
+++ src/modules/contentHub/components/InsightFilters/InsightFiltersDropdown.tsx
@@ -1,11 +1,15 @@
-/* eslint-disable jsx-a11y/interactive-supports-focus */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-import { TypeInsightTopic, TypeInsightType } from '@common/types/Insight'
-import { kebabCase } from 'lodash'
+// Packages
 import React, { useState } from 'react'
-import { useLayer } from 'react-laag'
+import { kebabCase } from 'lodash'
+
+// Icons
+import Arrow from '@static/svgs/common/arrow-simple-down.inline.svg'
+
+// Common
+import { TypeInsightTopic, TypeInsightType } from '@common/types/Insight'
 import { FilterState } from '.'
+
+// Styles
 import './style.css'
 
 interface Props {
@@ -13,8 +17,8 @@ interface Props {
   types: TypeInsightType[]
   topicsFilter: FilterState<TypeInsightTopic>
   typesFilter: FilterState<TypeInsightType>
-  createOnTopicClickHandler: (name: TypeInsightTopic) => void
-  createOnTypeClickHandler: (name: TypeInsightType) => void
+  createOnTopicClickHandler: (name: TypeInsightTopic) => () => void
+  createOnTypeClickHandler: (name: TypeInsightType) => () => void
 }
 
 const InsightFiltersDropdown = ({
@@ -28,93 +32,66 @@ const InsightFiltersDropdown = ({
   const [isTopicFilterOpen, setIsTopicFilterOpen] = useState(false)
   const [isTypeFilterOpen, setIsTypeFilterOpen] = useState(false)
 
-  const {
-    triggerProps: topicTriggerProps,
-    layerProps: topicLayerProps,
-    renderLayer: topicRenderLayer
-  } = useLayer({
-    isOpen: isTopicFilterOpen,
-    placement: 'bottom-center'
-  })
+  const handleToggleTopicFilter = () =>
+    setIsTopicFilterOpen((prevState) => !prevState)
 
-  const {
-    triggerProps: typeTriggerProps,
-    layerProps: typeLayerProps,
-    renderLayer: typeRenderLayer
-  } = useLayer({
-    isOpen: isTypeFilterOpen,
-    placement: 'bottom-center'
-  })
+  const handleToggleTypeFilter = () =>
+    setIsTypeFilterOpen((prevState) => !prevState)
 
   return (
-    <>
-      <div>
-        <p
-          className="border border-solid border-gray-300 text-caption font-extrabold text-gray-600 p-2"
-          onClick={() => setIsTopicFilterOpen(!isTopicFilterOpen)}
-          style={{ maxWidth: '100px' }}
-          {...topicTriggerProps}
-        >
-          Topic
+    <div className="flex items-center justify-between">
+      <div className="Insight-filters-dropdown">
+        <p className="text-caption" onClick={handleToggleTopicFilter}>
+          <span>Topic</span>
+          <Arrow />
         </p>
-        {isTopicFilterOpen &&
-          topicRenderLayer(
-            <div {...topicLayerProps} className=" bg-white px-2 pt-2 pb-4">
-              {topics.map((topic) => {
-                const handleOnClick = createOnTopicClickHandler(topic)
-                const isActive =
-                  !topicsFilter.noFilters &&
-                  topicsFilter.filters.includes(topic)
-                return (
-                  <div
-                    key={kebabCase(topic)}
-                    className={`Insight-filters-item ${
-                      isActive && 'is-active'
-                    }`}
-                    onClick={handleOnClick}
-                    role="button"
-                  >
-                    <span className="text-page-navigation">{topic}</span>
-                  </div>
-                )
-              })}
-            </div>
-          )}
+        {isTopicFilterOpen && (
+          <div className="Insight-filters-dropdown-menu">
+            {topics.map((topic) => {
+              const handleOnClick = createOnTopicClickHandler(topic)
+              const isActive =
+                !topicsFilter.noFilters && topicsFilter.filters.includes(topic)
+              return (
+                <div
+                  key={kebabCase(topic)}
+                  className={`Insight-filters-item ${isActive && 'is-active'}`}
+                  onClick={handleOnClick}
+                  role="button"
+                >
+                  <span className="text-page-navigation">{topic}</span>
+                </div>
+              )
+            })}
+          </div>
+        )}
       </div>
 
-      <div>
-        <p
-          className="border border-solid border-gray-300 text-caption font-extrabold text-gray-600 p-2"
-          onClick={() => setIsTypeFilterOpen(!isTypeFilterOpen)}
-          style={{ maxWidth: '100px' }}
-          {...typeTriggerProps}
-        >
-          Type
+      <div className="Insight-filters-dropdown">
+        <p className="text-caption" onClick={handleToggleTypeFilter}>
+          <span>Type</span>
+          <Arrow />
         </p>
-        {isTypeFilterOpen &&
-          typeRenderLayer(
-            <div {...typeLayerProps} className=" bg-white px-2 pt-2 pb-4">
-              {types.map((type) => {
-                const handleOnClick = createOnTypeClickHandler(type)
-                const isActive =
-                  !typesFilter.noFilters && typesFilter.filters.includes(type)
-                return (
-                  <div
-                    key={kebabCase(type)}
-                    className={`Insight-filters-item ${
-                      isActive && 'is-active'
-                    }`}
-                    onClick={handleOnClick}
-                    role="button"
-                  >
-                    <span className="text-page-navigation">{type}</span>
-                  </div>
-                )
-              })}
-            </div>
-          )}
+        {isTypeFilterOpen && (
+          <div className="Insight-filters-dropdown-menu">
+            {types.map((type) => {
+              const handleOnClick = createOnTypeClickHandler(type)
+              const isActive =
+                !typesFilter.noFilters && typesFilter.filters.includes(type)
+              return (
+                <div
+                  key={kebabCase(type)}
+                  className={`Insight-filters-item ${isActive && 'is-active'}`}
+                  onClick={handleOnClick}
+                  role="button"
+                >
+                  <span className="text-page-navigation">{type}</span>
+                </div>
+              )
+            })}
+          </div>
+        )}
       </div>
-    </>
+    </div>
   )
 }
 
