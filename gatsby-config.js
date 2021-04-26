40f6753efb4d3861f389f91c4bc2b28761bbb833
Vince Brown <vince@vincebrown.me>
2021-04-25 17:00:08 -0700

    WE-53 Style cleanup (#82)

--- gatsby-config.js
+++ gatsby-config.js
@@ -170,6 +170,13 @@ module.exports = {
         crossorigin: 'anonymous'
       }
     },
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        mergeLinkHeaders: false,
+        mergeCachingHeaders: false
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
