9b6c63f85ed78c332ebd8f1c05768693b764848d
Vince Brown <vince@vincebrown.me>
2021-09-26 09:31:27 -0700

    WE-115 GoSite Intro (#130)

---
+++ src/pages/case-studies/gosite.tsx
@@ -0,0 +1,18 @@
+// Packages
+import React from 'react'
+
+// Components
+import GoSiteIntro from '@modules/case-studies/gosite/components/GoSiteIntro'
+
+// Styles
+import '@common/styles/pages/gosite.css'
+
+const GoSite = () => {
+  return (
+    <div className="gosite">
+      <GoSiteIntro />
+    </div>
+  )
+}
+
+export default GoSite
