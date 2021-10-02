ddd7980c88c97eb42b41248dab9855cf623e38d9
Vince Brown <vince@vincebrown.me>
2021-10-02 07:18:47 -0700

    WE-117 Challenge and solution (#132)

---
+++ src/modules/case-studies/components/ChallengeSolution.tsx
@@ -0,0 +1,36 @@
+// Packages
+import React from 'react'
+
+// SVGs
+import ChallengeIcon from '@static/svgs/case-studies/challenge.inline.svg'
+import SolutionIcon from '@static/svgs/case-studies/solution.inline.svg'
+
+// Styles
+import '@modules/case-studies/components/styles/ChallengeSolution.css'
+
+interface Props {
+  challenge: React.ReactNode
+  solution: React.ReactNode
+}
+
+const ChallengeSolution = ({ challenge, solution }: Props) => {
+  return (
+    <div className="ChallengeSolution-container">
+      <div className="ChallengeSolution-container-inner">
+        <div className="ChallengeSolution-challenge">
+          <ChallengeIcon className="ChallengeSolution-challenge-icon" />
+          <h3 className="text-h3 font-extrabold mb-4">The Challenge</h3>
+          {challenge}
+        </div>
+
+        <div className="ChallengeSolution-solution">
+          <SolutionIcon className="ChallengeSolution-solution-icon" />
+          <h3 className="text-h3 font-extrabold mb-4">The Solution</h3>
+          {solution}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ChallengeSolution
