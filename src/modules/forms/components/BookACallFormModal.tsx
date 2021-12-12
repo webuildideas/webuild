cbedf1afd978cb3983675072493d1182dcad5d3a
Vince Brown <vince@vincebrown.me>
2021-12-12 00:00:43 -0800

    Close modals when background is pressed

--- src/modules/forms/components/BookACallFormModal.tsx
+++ src/modules/forms/components/BookACallFormModal.tsx
@@ -1,12 +1,14 @@
 // Packages
-import React, { memo } from 'react'
-import CloseIcon from '@static/svgs/closeIcon.inline.svg'
+import React, { useCallback, memo, useRef } from 'react'
 
 // Components
 import BookACallForm from '@modules/forms/BookACallForm'
 
 // Hooks
 import useBookACallFormModal from '@modules/forms/hooks/useBookACallFormModal'
+
+// Icon
+import CloseIcon from '@static/svgs/closeIcon.inline.svg'
 
 // Styles
 import '../styles/BookACallFormModal.css'
@@ -18,11 +20,31 @@ interface Props {
 const BookACallFormModal = memo(function BookACallFormModalMemo({
   location
 }: Props) {
+  const formRef = useRef<HTMLDivElement>(null)
   const { isVisible, closeModal } = useBookACallFormModal()
 
+  const handleBackgroundPress = useCallback(
+    (event) => {
+      if (
+        isVisible &&
+        formRef.current &&
+        formRef.current.contains(event.target)
+      ) {
+        return
+      }
+
+      closeModal()
+    },
+    [isVisible, formRef, closeModal]
+  )
+
   return isVisible ? (
-    <div className="BookACallFormModal">
-      <div className="BookACallFormModal-form bg-foundation">
+    <div
+      className="BookACallFormModal"
+      onClick={handleBackgroundPress}
+      role="button"
+    >
+      <div ref={formRef} className="BookACallFormModal-form bg-foundation">
         <CloseIcon
           className="BookACallFormModal-close text-gray-800"
           onClick={closeModal}
