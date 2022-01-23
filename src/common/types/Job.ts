345740638267b918817248d2b3b258971f4fd4e5
Vince Brown <vince@vincebrown.me>
2022-01-23 11:56:13 -0800

    Update job content type

--- src/common/types/Job.ts
+++ src/common/types/Job.ts
@@ -1,6 +1,10 @@
+import { TypeContentfulAsset } from './Contentful'
+
 export interface TypeJob {
   isOpen: boolean
   title: string
+  description: string
+  illustration: TypeContentfulAsset
   location: string
   applicationLink: string
 }
